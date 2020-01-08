f0deb794cd06647caf44ce982da64872601119ba
juanjot84 <jjtassin@hotmail.com>
2020-01-07 21:32:52 -0300

    Input sin funcionar header

--- 03-componentes/src/app/app-routing.module.ts
+++ 03-componentes/src/app/app-routing.module.ts
@@ -46,7 +46,11 @@ const routes: Routes = [
   {
     path: 'infinite-scroll',
     loadChildren: () => import('./pages/infinite-scroll/infinite-scroll.module').then( m => m.InfiniteScrollPageModule)
+  },  {
+    path: 'input',
+    loadChildren: () => import('./pages/input/input.module').then( m => m.InputPageModule)
   },
+
 ];
 
 @NgModule({
--- 03-componentes/src/app/pages/infinite-scroll/infinite-scroll.page.ts
+++ 03-componentes/src/app/pages/infinite-scroll/infinite-scroll.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-infinite-scroll',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./infinite-scroll.page.scss'],
 })
 export class InfiniteScrollPage implements OnInit {
+
+//  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
   data: any[] = Array(20);
 
@@ -21,12 +24,14 @@ export class InfiniteScrollPage implements OnInit {
 
       if ( this.data.length > 50) {
         event.target.complete();
+     //   this.infiniteScroll.disabled = true;
+        return;
       }
 
       const nuevoArr = Array(20);
       this.data.push(...nuevoArr);
       event.target.complete();
-    });
+    }, 1000 );
 
   }
 
