b68f1fae4dbfd6d4495d30011dedc584f3fc2e89
juanjot84 <jjtassin@hotmail.com>
2020-01-09 08:26:53 -0300

    fix de la aplicacion

--- 03-componentes/src/app/pages/modal-info/modal-info-routing.module.ts
+++ 03-componentes/src/app/pages/modal-info/modal-info-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { ModalInfoPage } from './modal-info.page';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: ModalInfoPage
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
